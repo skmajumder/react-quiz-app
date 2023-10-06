fc31d69f2442c6546d295ca07483afacbd12b333
skmajumder <skmajumder.cse@gmail.com>
2023-10-06 22:35:07 +0600

    Loading Questions from a Fake API and useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Invalid Action");
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status } = state;
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchQuestions() {
+      try {
+        const response = await fetch(`http://localhost:8000/questions`, {
+          signal: controller.signal,
+        });
+
+        if (!response.ok) {
+          throw new Error("Something went wrong with the fetching data");
+        }
+
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.log(error.message);
+          dispatch({ type: "dataFailed" });
+        }
+      }
+    }
+
+    fetchQuestions();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
